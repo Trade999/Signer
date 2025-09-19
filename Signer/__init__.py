90fb3e710bf044dc46cd76a269d697821e264dac
Trade999 <saf81181@gmail.com>
2025-09-19 12:47:28 +0300

    Update __init__.py

--- Signer/__init__.py
+++ Signer/__init__.py
@@ -1,8 +1,10 @@
 from .X_Gorgon import generate_gorgon
 from .X_ladon import x_ladon
 from .X_Argus import Argus
+from signature import signature
 __all__ = [
   "generate_gorgon",
   "x_ladon",
-  "Argus
+  "Argus,
+  "signature"
 ]
