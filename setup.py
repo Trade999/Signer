fcb870608a2a62ab187a1e81373ac0651b61b6f2
Trade999 <saf81181@gmail.com>
2025-09-19 12:52:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="Signer",
-    version="1.1",
+    version="1.2",
     description="Signer For TikTok",
     author="Lariot",
     author_email="lariot.antsa@gmail.com",
