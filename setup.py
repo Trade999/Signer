163aefe39dee94eea61a852335f15ed3ab7e5ec2
Trade999 <saf81181@gmail.com>
2025-09-19 07:22:01 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="Signer",
+    version="1.0",
+    description="Signer For TikTok",
+    author="Lariot",
+    author_email="lariot.antsa@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'report-tiktok=TikTok.main:main'
+        ]
+    },
+    python_requires=">=3.6",
+)
