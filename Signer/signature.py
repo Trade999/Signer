a684214f3f91178f8130cca64b26aca808a231e4
Trade999 <saf81181@gmail.com>
2025-09-26 14:46:33 +0300

    Add files via upload

--- Signer/signature.py
+++ Signer/signature.py
@@ -3,11 +3,11 @@ from .X_Gorgon import generate_gorgon
 from .X_ladon import x_ladon
 from .X_Argus import Argus
 
-def signature(params, payload: str = None, sec_device_id: str = "", cookie: str or None = None, aid: int = 1233, license_id: int = 1611921764, sdk_version_str: str = "v04.04.05-ov-android", sdk_version: int = 134744640, platform: int = 0, unix: int = None):
+def signature(params, payload: str = None, sec_device_id: str = "", cookie: str or None = None, aid: int = 1233, license_id: int = 1611921764, sdk_version_str: str = "2.3.1.i18n", sdk_version: int = 2, platform: int = 19, unix: int = None):
   x_ss_stub = hashlib.md5(payload.encode('utf-8')).hexdigest() if payload != None else None
   if not unix: unix = int(time.time())
   headers = generate_gorgon(params, payload, cookie)
-  headers.update(x_ladon(unix))
+  headers.update(x_ladon())
   xargus = {
     "x-argus": Argus.get_sign(
       params,
